c6dd645f9bc0896668558096c74ba8ae51cb20f2
Asttr0 <mohamedtahaslimani0@gmail.com>
2026-08-10 23:58:42 +0100

    formatted code by mistake

--- LeetCode/Easy/Valid_Parentheses/solution.ts
+++ LeetCode/Easy/Valid_Parentheses/solution.ts
@@ -1,26 +1,31 @@
 function isValid(s: string): boolean {
-
   const str = Array.from(s);
-  let charcheck = str.every(char => char == '[' || char == ']' || char == '{' || char == '}' || char == '(' || char == ')' );
+  let charcheck = str.every(
+    (char) =>
+      char == "[" ||
+      char == "]" ||
+      char == "{" ||
+      char == "}" ||
+      char == "(" ||
+      char == ")",
+  );
 
-  if (!charcheck) return false ;
+  if (!charcheck) return false;
 
-  const open = ['(' , '{' , '['];
-  const close = [')' , '}' , ']'];
+  const open = ["(", "{", "["];
+  const close = [")", "}", "]"];
 
-  for (let i = 0 ; i<str.length ; i++){
-
+  for (let i = 0; i < str.length; i++) {
     const closeindex = close.indexOf(str[i]);
 
-    if (closeindex !== -1){
-      if (i>0 && str[i-1] === open[closeindex]){
-        str.splice(i-1 , 2);
+    if (closeindex !== -1) {
+      if (i > 0 && str[i - 1] === open[closeindex]) {
+        str.splice(i - 1, 2);
         i -= 2;
       } else return false;
     }
   }
 
   return str.length === 0;
-
-};
+}
 console.log(isValid("{}[]()"));
