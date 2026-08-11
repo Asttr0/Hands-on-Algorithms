167b79080e84edd45b3e076fb00f0861aa1dc42f
Asttr0 <mohamedtahaslimani0@gmail.com>
2026-08-11 22:13:39 +0100

    ez

---
+++ LeetCode/Easy/Smallest_Missing_Integer_Greater_Than_Sequential_Prefix_Sum/solutions.ts
@@ -0,0 +1,20 @@
+function missingInteger(nums: number[]): number {
+    const n = nums.length ;
+    let counter = nums[0];
+
+    for ( let i = 0 ; i < n ; i++){
+      if (nums[i] == nums[i+1]-1){
+        counter += nums[i+1];
+      } else break;
+  }
+  const numSet = new Set(nums);
+
+
+    while (numSet.has(counter)) {
+        counter++;
+    }
+
+    return counter;
+};
+
+console.log(missingInteger([3,4,5,1,12,14,13]));
